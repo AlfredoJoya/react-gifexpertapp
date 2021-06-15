8b0cf8c1990bb206098a113461db9cd3c804587d
Alfredo Joya <alfredo.joya05@gmail.com>
2021-06-15 00:47:57 -0600

    First commit

---
+++ src/helpers/getGifs.js
@@ -0,0 +1,22 @@
+
+
+
+export const getGifs = async ( category ) => {
+
+    const url =`https://api.giphy.com/v1/gifs/search?q=${encodeURI( category )}&limit=20&api_key=50Z6ksJLjwdOLwC4G0OcJCWq3Uq6lqrn`
+    const resp = await fetch( url );
+    const { data }= await resp.json();
+
+    const gifs = data.map( img => {
+
+        return {
+            id: img.id,
+            title: img.title,
+            url: img.images?.downsized_medium.url
+            
+        }
+
+    })
+
+    return gifs;
+}
